93cfa4a04cbb1071a3c811907ad780e0f8f0f4c9
Kevin Thomas <kevin@one34.com>
2014-02-18 22:40:43 -0500

    Completed arrays

--- app/objects.js
+++ app/objects.js
@@ -3,8 +3,8 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
-    },
+		fn.bind(obj);
+	},
 
     alterObjects : function(constructor, greeting) {
 
