c095af72f9493da1560d7b518efcac0c376a2ca2
Kevin Thomas <kevinthomas1989@live.com>
2015-10-10 16:56:49 -0400

    async method implementations
    * Use ES6 Promise API for promises
    * Use Fetch API for AJAX calls

--- app/async.js
+++ app/async.js
@@ -2,10 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function (resolve, reject) {
+        resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
-
+    return fetch('/data/testdata.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseObject) {
+            return responseObject.people.reduce(function (prev, current) {
+                prev.push( current.name ); return prev;
+            }, []).sort();
+        });
   }
 };
